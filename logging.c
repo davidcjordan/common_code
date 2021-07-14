932b6477752293e5b9eab0bb8a1a46a369dee6a6
manningt <tom@manningetal.com>
2021-07-14 10:36:19 -0400

    only use shm log; no memory log

--- logging.c
+++ logging.c
@@ -3,8 +3,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h> //for exit
+#include <string.h>
 #include <time.h> //for timestamping log
+#include <libgen.h> //for basename
 #include "logging.h"
 #include "timer_functions.h"
 
@@ -12,11 +13,148 @@
 #define LOG_FILENAME "/run/shm/boomer.log"
 #define PREVIOUS_LOG_FILENAME "/run/shm/previous_boomer.log"
 #define LOG_FILE_MAX_BYTES 6000000
-#define LOG_FLUSH_INTERVAL_NANOS 1E9
+#define LOG_FLUSH_INTERVAL_MILLIS 1E7
 //#define TIME_BASED_FFLUSH
 
 FILE *pFile;
 uint64_t previous_flush_timestamp;
+uint32_t log_start_pos;
+const char LEVEL_FATAL[] = "FATL";
+const char LEVEL_ERROR[] = "INFO";
+const char LEVEL_WARN[] = "WARN";
+const char LEVEL_INFO[] = "INFO";
+const char LEVEL_DEBUG[] = "DBUG";
+
+uint32_t current_log_level = 5;
+
+void logging_init(void){
+//	debug_log_write_ptr = &debug_log[0];
+	#ifdef LOG_TO_SHM
+	pFile=fopen(LOG_FILENAME, "a+");
+	if(pFile==NULL)
+	{
+   	perror("Error opening /run/shm log file\n");
+		//exit(1);
+	}
+	fprintf(pFile, "\n");
+	// with a+ (append plus read) need to do a write to get a position at the end of the file
+	log_start_pos = ftell(pFile);
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	fprintf(pFile, "==== Log opened at: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	previous_flush_timestamp = counter();
+	#endif
+}
+
+void log_main(int level, const char * filename, int line_num, const char * format, ...) 
+{
+	if (level > current_log_level) return;
+
+   const char * level_ptr = LEVEL_DEBUG;
+   if (level == LLOG_FATAL) level_ptr = LEVEL_FATAL;
+   else if (level == LLOG_ERROR) level_ptr = LEVEL_ERROR;
+   else if (level == LLOG_WARN) level_ptr = LEVEL_WARN;
+   else if (level == LLOG_INFO) level_ptr = LEVEL_INFO;
+
+   char info_string[96]; 
+   struct timespec curTime; struct tm* info; 
+   clock_gettime(CLOCK_REALTIME, &curTime); 
+   info = localtime(&curTime.tv_sec); 
+   sprintf(info_string, "%d-%02d-%02dT%02d:%02d:%02d.%d_%s_%.10s-L%03d", 1900 + info->tm_year, info->tm_mon, info->tm_mday, \
+      info->tm_hour, info->tm_min, info->tm_sec, (int) (curTime.tv_nsec/1000000), 
+		level_ptr, basename((char * )filename), line_num); 
+   
+   char log_string[128]; 
+   va_list aptr; 
+   va_start(aptr, format); 
+   vsnprintf(log_string, sizeof(log_string), format, aptr); 
+   va_end(aptr);
+
+   if (ftell(pFile) > LOG_FILE_MAX_BYTES){
+		fclose(pFile);
+		remove(PREVIOUS_LOG_FILENAME);
+		if (rename(LOG_FILENAME, PREVIOUS_LOG_FILENAME) != 0) printf("Error: boomer_log rename failed");
+		pFile=fopen(LOG_FILENAME, "w+");
+		if(pFile==NULL)
+		{
+			perror("Error opening /run/shm log file\n");
+		}
+      log_start_pos = ftell(pFile);
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+		fprintf(pFile, "==== Log rolled at: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	}
+	fprintf(pFile, "%s>%s\n", info_string, log_string);
+	// fflush( pFile );
+
+	if((counter() - previous_flush_timestamp) > LOG_FLUSH_INTERVAL_MILLIS){
+		// time_t t = time(NULL);
+		// struct tm tm = *localtime(&t);
+		// printf("==== flushing at: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+		fflush( pFile );
+		previous_flush_timestamp = counter();
+	}
+}
+
+void save_debug_log(char*path){
+   fflush(pFile);
+   uint32_t log_end_pos = ftell(pFile);
+   uint32_t log_size = log_end_pos - log_start_pos;
+	bool file_write = strcmp(path, "stdout");
+	#define MAX_STDOUT_CHARS 32*1024
+	//limit printf output
+	if (!file_write && log_size > MAX_STDOUT_CHARS) log_size > MAX_STDOUT_CHARS;
+
+   //printf("log_start: %d log_end: %d; size: %d\n", log_start_pos, log_end_pos, log_size);
+   char buffer[4096*1024];
+   size_t rw_size;
+   #define ELEMENTS_TO_READ 1
+   if (log_size < sizeof(buffer)) {
+      int rc = fseek(pFile, log_start_pos, SEEK_SET);
+      if (rc != 0) 
+      {
+         perror("debug_log_save: seek to log_start failed");
+         return;
+      }
+      rw_size = fread(buffer, log_size, ELEMENTS_TO_READ, pFile);
+		fflush(stdout);
+      if (rw_size != ELEMENTS_TO_READ) {
+         printf("%s read of %d bytes failed.", LOG_FILENAME, log_size);
+			fflush(stdout);
+      } else
+      {
+			if (file_write) {
+				FILE *f = fopen(path,"w");
+				rw_size = fwrite(buffer, log_size, ELEMENTS_TO_READ, f);
+				if (rw_size != ELEMENTS_TO_READ) {
+					printf("%s write of %d bytes failed.", path, log_size);
+					fflush(stdout);
+				}
+				fclose(f);
+			}
+			else {
+				printf(buffer); printf("\n"); fflush(stdout);
+			}
+      }
+      // restore append position
+      rc = fseek(pFile, 0, SEEK_END);
+      if (rc != 0) 
+      {
+         perror("save_debug_log: seek to end failed");
+      }
+    } else {
+		LOG_ERROR("log_size of %d larger than save buffer.\n", log_size);
+      printf("log_size of %d larger than save buffer.\n", log_size);
+   }
+}
+
+void clear_debug_log(){
+	log_start_pos = ftell(pFile);
+}
+void print_debug_log(){
+   save_debug_log("stdout");
+}
+/*
 
 //the debug log is a text buffer holding 1MB of text
 //print to it as you would to the console using LOG_DEBUG()
@@ -25,25 +163,6 @@ char debug_log[debug_log_length];
 char* debug_log_write_ptr; //points to the next space to write to in the debug log
 const char* debug_log_end_ptr = &debug_log[debug_log_length-1]; //last valid debug log pointer
 bool debug_log_filled = false; //lets us know when the whole log is full of valid data
-
-//prototypes
-void VA_LOG_DEBUG(char * message,va_list ap);
-
-void logging_init(void){
-	debug_log_write_ptr = &debug_log[0];
-	#ifdef LOG_TO_SHM
-	pFile=fopen(LOG_FILENAME, "a");
-	if(pFile==NULL)
-	{
-   	perror("Error opening /run/shm log file\n");
-		exit(1);
-	}
-	time_t t = time(NULL);
-	struct tm tm = *localtime(&t);
-	fprintf(pFile, "==== Log opened at: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-	previous_flush_timestamp = counter();
-	#endif
-}
 
 void LOG_ERROR(char *  message,...) {
 	va_list ap; //create pointer for argument list
@@ -124,13 +243,12 @@ void VA_LOG_DEBUG(char * message,va_list ap){ //same as log debug, but takes a v
 	#endif //log to shared memory file
 }
 
-//clears the debug log
-void clear_debug(){
+void clear_debug_log(){
 	debug_log_filled = false;
 	debug_log_write_ptr = &debug_log[0];
 }
 
-void print_debug(){
+void print_debug_log(){
 	char* read_ptr;
 	if(!debug_log_filled){
 		read_ptr = &debug_log[0];
@@ -150,7 +268,7 @@ void print_debug(){
 	}
 }
 
-void save_debug(char *path)
+void save_debug_log(char *path)
 {
 	char *read_ptr;
 	FILE *f = fopen(path, "w");
@@ -188,3 +306,4 @@ void save_debug(char *path)
 	}
 	fclose(f);
 }
+*/
--- logging.h
+++ logging.h
@@ -1,14 +1,32 @@
-#pragma once
+#ifndef logging_h
+#define logging_h
 
 void logging_init();
-
+/*
 void LOG_ERROR(char*message,...);
 void LOG_WARNING(char*message,...);
 void LOG_DEBUG(char*message,...);
 void LOG_INFO(char*message,...);
+*/
 
-void print_debug(); //prints out the debug log
+#define LLOG_FATAL (1)
+#define LLOG_ERROR (2)
+#define LLOG_WARN  (3)
+#define LLOG_INFO  (4)
+#define LLOG_DEBUG (5)
 
-void clear_debug(); //clears the debugging log
+void log_main(int level, const char * filename, int line_num, const char * format, ...);
 
-void save_debug(char*path);
+#define LOG_FATAL(...)  log_main(LLOG_FATAL, __BASE_FILE__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...)  log_main(LLOG_ERROR, __BASE_FILE__, __LINE__, __VA_ARGS__)
+#define LOG_WARNING(...) log_main(LLOG_WARN, __BASE_FILE__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...)   log_main(LLOG_INFO, __BASE_FILE__, __LINE__, __VA_ARGS__)
+#define LOG_DEBUG(...)  log_main(LLOG_DEBUG, __BASE_FILE__, __LINE__, __VA_ARGS__)
+
+void print_debug_log();
+
+void clear_debug_log();
+
+void save_debug_log(char*path);
+
+#endif //logging.h
